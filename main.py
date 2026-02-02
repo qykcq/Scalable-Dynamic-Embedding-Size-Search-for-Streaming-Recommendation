263173777de70114efa67be9a00e0307be544fe8
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-02-02 10:18:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from Engine import Engine
 
 
 if __name__ == '__main__':
-    for ID in [0, 1]:
+    for ID in [1]:
         print('########################### ID = {} ###########################'.format(ID))
         engine = Engine(ID)
 
         engine.streaming_size_search()
+
